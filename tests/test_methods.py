b90f2ffbecf2bd6150cbe16d4ef8b12b60fe8d2a
Remi Gau <remi_gau@hotmail.com>
2022-12-15 23:09:51 +0100

    fix typo

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -9,8 +9,8 @@ from bidsmreye.methods import methods
 def test_methods():
 
     output_dir = Path.cwd().joinpath("temp")
-    output_dir = methods(output_dir)
+    output_file = methods(output_dir)
 
-    assert output_dir.is_file()
+    assert output_file.is_file()
 
     shutil.rmtree(output_dir)
